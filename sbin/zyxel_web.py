f246c92ec2920ba61d3d1681a8d21f86597f5f16
Michiel Holtkamp <git@supermind.nl>
2013-01-11 05:00:52 +0100

    Incorporated lan/nat stats into zyxel script

--- sbin/zyxel_web.py
+++ sbin/zyxel_web.py
@@ -4,6 +4,18 @@ import requests
 from pyquery import PyQuery
 import sys
 
+def strip_hr(text):
+	"""Strip human readable string from number of bytes. Expecting text in
+	the form '3375430055(3.4G)' or '0'"""
+	try:
+		lparen = text.index('(')
+	except ValueError:
+		pass
+	else:
+		text = text[:lparen]
+
+	return text.strip()
+
 class Zyxel():
 	def __init__(self, hostname, username, password):
 		self.url = "http://" + hostname + "/"
@@ -28,7 +40,20 @@ class Zyxel():
 		for tr in p("table.table_frame > tr"):
 			row = [td for td in PyQuery(tr).find('td.table_font')]
 			if len(row) > 0:
-				print '\t'.join([x.text for x in row])
+				print '\t'.join([x.text.replace(' ', '-') for x in row])
+
+	def lan(self):
+		r = self.s.post(self.url + "lan_status.cgi")
+
+		p = PyQuery(r.text)
+		nat = {}
+		trs = p("table.table_frame > tr")
+		interfaces = [td.text.strip() for td in PyQuery(trs[0]).find('td.top_font')]
+		sent = [strip_hr(td.text) for td in PyQuery(trs[1]).find('td.table_font')]
+		recv = [strip_hr(td.text) for td in PyQuery(trs[2]).find('td.table_font')]
+		for row in zip(interfaces[1:], recv, sent):
+			print "%sin.value %s" % (row[0], row[1])
+			print "%sout.value %s" % (row[0], row[2])
 
 	def diagnostics(self):
 		payload = {
@@ -40,6 +65,7 @@ class Zyxel():
 		# get the textarea with the info
 		p = PyQuery(r.text)
 		print p("div.infodisplay > textarea").text()
+
 
 if __name__ == "__main__":
 	if len(sys.argv) < 5:
@@ -53,3 +79,5 @@ if __name__ == "__main__":
 		z.nat()
 	elif cmd == "diagnostics":
 		z.diagnostics()
+	elif cmd == "lan":
+		z.lan()
